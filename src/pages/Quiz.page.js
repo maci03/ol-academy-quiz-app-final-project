a4808cd02632eb05536df0ecb034ce0c451f7ea9
Maci Abesadze <maci.abesadze@gmail.com>
2022-12-02 01:26:44 +0300

    Implement fetching functionality and start working on quiz timer

--- src/pages/Quiz.page.js
+++ src/pages/Quiz.page.js
@@ -1,7 +1,71 @@
+import { useEffect, useState } from "react"
+const URL = "http://my-json-server.typicode.com/DanielBarbakadze/Advanced-JS-and-React-Basics/db"
+
+function getWithExpiry() {
+  const savedQuizStr = localStorage.getItem('fetchedQuiz');
+
+  if (!savedQuizStr) {
+    return null;
+  };
+
+  const now = new Date();
+
+  // { data: "", expiry: ""}
+
+  const savedQuiz = JSON.parse(savedQuizStr);
+
+  if (now.getTime() > savedQuiz.expiry) {
+    localStorage.removeItem('fetchedQuiz');
+    return null;
+  } else {
+    console.log('quiz exists', savedQuiz);
+    return savedQuiz;
+  }
+};
+
+const fetchQuestions = async (url) => {
+  try {
+    const response = await fetch(url);
+    const parsedData = await response.json();
+
+    const answers = parsedData.answers || [];
+    const questions = parsedData.questions || [];
+
+    const desiredData = questions.map(question => {
+      const updatedQuestion = { ...question };
+      const questionAnswerData = answers.find(answer => answer.id === question.id);
+
+      updatedQuestion.answer = questionAnswerData.answer;
+      return updatedQuestion;
+    })
+
+    return desiredData;
+  } catch (error) {
+    return error;
+  }
+};
+
 const Quiz = () => {
-    return (
-        <div>Quiz page</div>
-    )
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestionIdx, setCurrentQuestionIdx] = useState(0);
+
+  useEffect(() => {
+    // const savedData = getWithExpiry();
+
+    // if (savedData) {
+    //   setQuestions(savedData);
+    //   return;
+    // }
+
+    fetchQuestions(URL)
+      .then(res => {
+        console.log('desired data', res)
+      })
+  }, [])
+
+  return (
+    <div>Quiz page</div>
+  )
 }
 
 export default Quiz
